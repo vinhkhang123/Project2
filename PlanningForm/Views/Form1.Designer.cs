1cb6f8f065192271d22e947dec7ba341d5fe1cfe
vinhkhang123 <wholaughs666@gmail.com>
2024-02-19 17:28:53 +0700

    sửa lại form

--- PlanningForm/Views/Form1.Designer.cs
+++ PlanningForm/Views/Form1.Designer.cs
@@ -37,7 +37,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.control_panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -129,7 +128,6 @@
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.btn_table4);
             this.panel2.Controls.Add(this.btn_table1);
             this.panel2.Controls.Add(this.btn_table2);
@@ -149,18 +147,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1106, 683);
             this.panel1.TabIndex = 6;
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(0, 327);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(239, 81);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Excel";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -193,7 +179,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
